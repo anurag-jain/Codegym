c4d9c728ecd7bcd3f2d4fcf4c74e0d39ea323f94
Anurag Jain <anuragjainfzd@gmail.com>
2014-02-27 21:35:52 +0530

    Basic insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -6,38 +6,61 @@
 #include <vector>
 #include <iterator>
 
-
+#define DEFAULT_INPUT_SIZE 10
 
 using namespace std;
 
-void insertSort();
-
-
-
-int main()
+template <class T>
+void insertSort(vector<T> &vec)
 {
-	//Generate random data
-	//
-	char *a = "12sjdfjs";
-	char *b = "jfkfs109";
-	char *c = "1234";
-	char *d = "-123";
-	char *e = "-123asd";
-	char *f = "3782849328498249829849283498239482984983294832984283498324983294829";
-	char *t;
+	T pivot;
+	int j;
+	for(long i=1;i<vec.size();i++)
+	{
+		pivot 	= vec[i];
+		j 		= i-1;
+		while(j>=0 && vec[j] > pivot)
+		{
+			vec[j+1] = vec[j];
+			j--;
+		}
+		vec[j+1] = pivot;
+	}
+}
 
-	printf("%ld\n", strtol(a,&t,0));
-	printf("%d %d\n", a, t);
-	printf("%ld\n", strtol(b,&t,0));
-	printf("%d %d\n", b, t);
-	printf("%ld\n", strtol(c,&t,0));
-	printf("%d %d\n", c, t);
-	printf("%ld\n", strtol(d,&t,0));
-	printf("%d %d\n", d, t);
-	printf("%ld\n", strtol(e,&t,0));
-	printf("%d %d\n", e, t);
-	printf("%ld\n", strtol(f,&t,0));
-	printf("%d %d\n", f, t);
+
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		cout<<"USAGE: insertion_sort <number of elements>"<<endl;
+		exit(1);
+	}
+	char *endPtr;
+	long numElement = strtol(argv[1], &endPtr, 0);
+	if(numElement == 0)
+		numElement=DEFAULT_INPUT_SIZE;
+
+	std::vector<int> inputVector;
+
+	//inputVector.reserve(numElement);
+
+	srand(time(NULL));
+	while(numElement--)
+	{
+		printf("%ld\n", numElement);
+		inputVector.push_back(rand()%32767);
+	}
+
+	insertSort(inputVector);
+
+	for (int i = 0; i < inputVector.size(); ++i)
+	{
+		printf("%d\n", inputVector[i]);
+	}
+
+
 
 	return 0;
 }
